0b48e0b2567e65070453127f601366b75388cc69
Claudi <claudi@opportunityatwork.org>
2022-07-10 12:40:16 -0300

    Adiciona arquivo de exemplo para "Boolean"

---
+++ exercicios/02_Data_Types/02_Booleans.rb
@@ -0,0 +1,26 @@
+# docker-compose run --rm ruby_app ruby exercicios/02_Data_Types/02_Booleans.rb
+puts "\n>>>>>Criar variáveis \n\n"
+
+puts "\nTrueClass: \n"
+temperatura = 30
+estaQuente = temperatura > 25
+frase = "Está quente com a temperatura em #{temperatura}°C? #{estaQuente}"
+puts frase
+puts("estaQuente.class -> #{estaQuente.class}")
+puts("estaQuente.class.superclass -> #{estaQuente.class.superclass}")
+puts("estaQuente.class.superclass.superclass -> #{estaQuente.class.superclass.superclass}")
+puts("estaQuente.class.superclass.superclass.superclass -> #{estaQuente.class.superclass.superclass.superclass}")
+
+puts "\nFalseClass: \n"
+temperatura = 20
+estaQuente = temperatura > 25
+frase = "Está quente com a temperatura em #{temperatura}°C? #{estaQuente}"
+puts frase
+puts("estaQuente.class -> #{estaQuente.class}")
+puts("estaQuente.class.superclass -> #{estaQuente.class.superclass}")
+puts("estaQuente.class.superclass.superclass -> #{estaQuente.class.superclass.superclass}")
+puts("estaQuente.class.superclass.superclass.superclass -> #{estaQuente.class.superclass.superclass.superclass}")
+
+# Referências:
+# https://ruby-doc.org/core-3.1.2/TrueClass.html
+# https://ruby-doc.org/core-3.1.2/FalseClass.html
